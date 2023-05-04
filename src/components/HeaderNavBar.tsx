b185b732905a5bb6c25f68bfdab5ae2e252c5aee
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2023-05-04 10:16:04 +0200

    feat(ui): Add navigation bar and change layout

---
+++ src/components/HeaderNavBar.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import Link from "next/link";
+
+export default function HeaderNavBar() {
+    return (
+        <nav className="navigation">
+            <Link href="/" className="brand-name">
+                Kite Spots
+            </Link>
+            <button className="hamburger">
+                {/* icon from heroicons.com */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="white"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+            </button>
+            <div
+                className="navigation-menu">
+                <ul className={"flex gap-4 mr-4"}>
+                    <li>
+                        <Link href="/home">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
