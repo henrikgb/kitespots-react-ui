f19137b726f592d2e7ed8accf7dea87bf59517df
henrikgb <henrik-gb@hotmail.com>
2023-05-05 13:45:39 +0200

    feat(ui): Change height of map

--- src/components/MyMap.tsx
+++ src/components/MyMap.tsx
@@ -1,13 +1,11 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import {Icon} from "leaflet";
-import {useState} from "react";
 
 const MyMap = () => {
-    const [isNavExpanded] = useState(false)
     return (
         <MapContainer className={"absolute z-0"} center={[58.80636404000909, 5.724361473212989]}
-                      zoom={9} scrollWheelZoom={true} style={{height: "100vh", width: "100vw"}}>
+                      zoom={9} scrollWheelZoom={true} style={{height: "92vh", width: "100vw"}}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
