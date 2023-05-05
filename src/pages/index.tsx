889ea8dfe030ac6928e20939a8787ec112d81f0f
henrikgb <henrik-gb@hotmail.com>
2023-05-05 11:48:30 +0200

    feat(ui): Add map with leaflet react

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import HeaderNavBar from "@/components/HeaderNavBar";
+import dynamic from "next/dynamic";
+import React from "react";
 
 export default function Home() {
+    const DynamicMap = React.useMemo(() => dynamic(
+        () => import("../components/MyMap"), // replace '@components/map' with your component's location
+        {
+            loading: () => <p>A map is loading</p>,
+            ssr: false // This line is important. It's what prevents server-side render
+        }
+    ), [/* list variables which should trigger a re-render here */]);
   return (
     <>
       <Head>
@@ -14,7 +23,9 @@ export default function Home() {
 
         <div className={styles.main}>
             <HeaderNavBar />
-            <div className={"max-h-full max-w-full bg-amber-400 page-body"}></div>
+            <div className={"max-h-full max-w-full bg-amber-400 page-body"}>
+                <DynamicMap />
+            </div>
         </div>
     </>
   )
