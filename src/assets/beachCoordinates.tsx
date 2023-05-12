3f16ee42251f0a8860831a01dd892933fbb01eea
henrikgb <henrik-gb@hotmail.com>
2023-05-12 23:00:11 +0200

    Refactor: Add a file with a list of objects representing the various beach spots with their coordinates and some relevant information. Loop through this list of objects when adding them on the map.

---
+++ src/assets/beachCoordinates.tsx
@@ -0,0 +1,56 @@
+export const beachCoordinates = [
+  {
+    "id": 1,
+    "latitude": 59.02050003940309,
+    "longitude": 5.592325942611728,
+    "info": "Sandestranden, Randaberg. Nice on days with onshore wind. Gusty on side shore wind. Usually flat and no waves."
+  },
+  {
+    "id": 2,
+    "latitude": 58.88531361351894,
+    "longitude": 5.602662428854268,
+    "info": "Solastranden, Sola. Nice on days with onshore wind. Gusty on side shore wind. Usually flat and no waves."
+  },
+  {
+    "id": 3,
+    "latitude": 58.84227538997773,
+    "longitude": 5.560516132410946,
+    "info": "Hellestøstranden, Sola."
+  },
+  {
+    "id": 4,
+    "latitude": 58.81231852222533,
+    "longitude": 5.546945324943648,
+    "info": "Selestranda, Klepp. Good spot for several wind directions. Potentially large waves."
+  },
+  {
+    "id": 5,
+    "latitude": 58.80123195118268,
+    "longitude": 5.5480941336096645,
+    "info": "Borestranden, Klepp. Good spot for several wind directions. Potentially large waves."
+  },
+  {
+    "id": 6,
+    "latitude": 58.740441600947264,
+    "longitude": 5.512925570900187,
+    "info": "Orrestranda. Good spot for onshore and south wind directions. Potentially large waves."
+  },
+  {
+    "id": 7,
+    "latitude": 58.722027,
+    "longitude": 5.521960,
+    "info": "X. Good spot for onshore and south wind directions. Potentially large waves."
+  },
+  {
+    "id": 8,
+    "latitude": 58.68756890551574,
+    "longitude": 5.551150818355702,
+    "info": "Refsnesstranden. Good spot for onshore and south wind directions. Potentially large waves."
+  },
+  {
+    "id": 9,
+    "latitude": 58.53797648002299,
+    "longitude": 5.730672797723366,
+    "info": "Brusand strand. Good spot for onshore and south wind directions. Potentially large waves."
+  }
+]
--- src/components/MyMap.tsx
+++ src/components/MyMap.tsx
@@ -1,8 +1,17 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import {Icon} from "leaflet";
+import {beachCoordinates} from "@/assets/beachCoordinates";
 
 const MyMap = () => {
+
+    const markers = beachCoordinates.map((beachCoordinates) => (
+       <Marker key={beachCoordinates.id} position={[beachCoordinates.latitude, beachCoordinates.longitude]}
+               icon={new Icon({iconUrl: "pin.png", iconSize: [25, 41], iconAnchor: [12, 41]})} >
+           <Popup>{beachCoordinates.info}</Popup>
+       </Marker>
+    ));
+
     return (
         <MapContainer className={"absolute z-0"} center={[58.80636404000909, 5.724361473212989]}
                       zoom={9} scrollWheelZoom={true} style={{height: "92vh", width: "100vw"}}>
@@ -10,63 +19,7 @@ const MyMap = () => {
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker key="1" position={[59.02050003940309, 5.592325942611728]}
-                    icon={new Icon({iconUrl: "pin.png", iconSize: [25, 41], iconAnchor: [12, 41]})} >
-                <Popup>
-                    Sandestranden, Randaberg. <br /> Nice on days with onshore wind. <br />
-                    Gusty on side shore wind. <br/> Usually flat and no waves.
-                </Popup>
-            </Marker>
-            <Marker key="2"  position={[58.88531361351894, 5.602662428854268]}
-                    icon={new Icon({iconUrl: "pin.png", iconSize: [25, 41], iconAnchor: [12, 41]})} >
-                <Popup>
-                    Solastranden, Sola. <br /> Nice on days with onshore wind. <br />
-                    Gusty on side shore wind. <br/> Usually flat and no waves.
-                </Popup>
-            </Marker>
-            <Marker key="3"  position={[58.84227538997773, 5.560516132410946]}
-                    icon={new Icon({iconUrl: "pin.png", iconSize: [25, 41], iconAnchor: [12, 41]})} >
-                <Popup>
-                    Hellestøstranden, Sola.
-                </Popup>
-            </Marker>
-
-            <Marker key="4"  position={[58.81231852222533, 5.546945324943648]}
-                    icon={new Icon({iconUrl: "pin.png", iconSize: [25, 41], iconAnchor: [12, 41]})} >
-                <Popup>
-                    Selestranda, Klepp. <br /> Good spot for several wind directions. <br/> Potentially large waves.
-                </Popup>
-            </Marker>
-            <Marker key="5"  position={[58.80123195118268, 5.5480941336096645]}
-                    icon={new Icon({iconUrl: "pin.png", iconSize: [25, 41], iconAnchor: [12, 41]})} >
-                <Popup>
-                    Borestranden, Klepp. <br /> Good spot for several wind directions. <br/> Potentially large waves.
-                </Popup>
-            </Marker>
-            <Marker key="6"  position={[58.740441600947264, 5.512925570900187]}
-                    icon={new Icon({iconUrl: "pin.png", iconSize: [25, 41], iconAnchor: [12, 41]})} >
-                <Popup>
-                    Orrestranda. <br /> Good spot for onshore- <br/> and south wind directions. <br/> Potentially large waves.
-                </Popup>
-            </Marker>
-            <Marker key="7"  position={[58.722027, 5.521960]}
-                    icon={new Icon({iconUrl: "pin.png", iconSize: [25, 41], iconAnchor: [12, 41]})} >
-                <Popup>
-                    X. <br />  Good spot for onshore- <br/> and south wind directions. <br/> Potentially large waves.
-                </Popup>
-            </Marker>
-            <Marker key="8"  position={[58.68756890551574, 5.551150818355702]}
-                    icon={new Icon({iconUrl: "pin.png", iconSize: [25, 41], iconAnchor: [12, 41]})} >
-                <Popup>
-                    Refsnesstranden. <br /> Good spot for onshore- <br/> and south wind directions. <br/> Potentially large waves.
-                </Popup>
-            </Marker>
-            <Marker key="9"  position={[58.53797648002299, 5.730672797723366]}
-                    icon={new Icon({iconUrl: "pin.png", iconSize: [25, 41], iconAnchor: [12, 41]})} >
-                <Popup>
-                    Brusand strand . <br /> Good spot for onshore- <br/> and south wind directions. <br/> Potentially large waves.
-                </Popup>
-            </Marker>
+            {markers}
         </MapContainer>
     );
 };
