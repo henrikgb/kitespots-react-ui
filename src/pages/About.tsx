48115e5070e6407f8a07cd92600888efd67b6ba3
HBU <henrik-gb@hotmail.com>
2023-09-15 14:58:47 +0200

    eslint formatting

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,22 +1,23 @@
 import styles from "@/styles/Home.module.css";
 import Head from 'next/head'
 import HeaderNavBar from "@/components/HeaderNavBar";
-import React from "react";
 import {AboutContent} from "@/components/AboutContent";
 
 export default function About() {
-    return (
-        <>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <div className={styles.main}>
-                <HeaderNavBar />
-                <div className={"max-h-full max-w-full bg-amber-100 page-body"}>
-                    <AboutContent />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <HeaderNavBar />
+        <div className={"max-h-full max-w-full bg-amber-100 page-body"}>
+          <AboutContent />
+          <div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,22 +1,21 @@
 import Head from 'next/head'
 import HeaderNavBar from "@/components/HeaderNavBar";
-import React from "react";
 import {ContactContent} from "@/components/ContactContent";
 import styles from "@/styles/Home.module.css";
 
 export default function Contact() {
-    return (
-        <>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <div className={styles.main}>
-                <HeaderNavBar />
-                <div className={"max-h-full max-w-full bg-amber-100 page-body"}>
-                    <ContactContent />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <HeaderNavBar />
+        <div className={"h-fit w-full bg-amber-100 page-body"}>
+          <ContactContent />
+        </div>
+      </div>
+    </>
+  )
 }
