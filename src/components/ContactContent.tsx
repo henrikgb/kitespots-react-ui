3bfaca25449c24d83caa87140ff97610fa73af29
HBU <henrik-gb@hotmail.com>
2023-09-15 15:59:48 +0200

    Updating text in ContactContent.tsx

--- src/components/ContactContent.tsx
+++ src/components/ContactContent.tsx
@@ -1,7 +1,11 @@
 export function ContactContent() {
   return (
     <div className={"container bg-amber-200"}>
-      <h1 className={"font-bold"}>Contact info..</h1>
+      <h1 className={"font-bold"}>Kontakt info</h1>
+      <p>Har du spørsmål angående nettsiden<br /> eller ønsker du å legge til koordinater og værdata for flere lokasjoner?
+        <br /> Ikke nøl med å ta kontakt. Send en epost til
+        <a href="mailto:henrik-gb@hotmail.com" className="font-bold text-green-700"> henrik-gb@hotmail.com</a>, og jeg vil svare deg så snart som mulig.
+      </p>
     </div>
   )
 }
