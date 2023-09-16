1330d3fb0c099e30194867fa0568cb4fd0385406
HBU <henrik-gb@hotmail.com>
2023-09-16 12:34:54 +0200

    eslint formatting of various files and remove <Head> section in pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,23 +1,16 @@
 import styles from "@/styles/Home.module.css";
-import Head from 'next/head'
 import HeaderNavBar from "@/components/HeaderNavBar";
 import {AboutContent} from "@/components/AboutContent";
 
 export default function About() {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className={styles.main}>
-        <HeaderNavBar />
-        <div className={"max-h-full max-w-full bg-amber-100 page-body"}>
-          <AboutContent />
-          <div>
-          </div>
+    <div className={styles.main}>
+      <HeaderNavBar />
+      <div className={"max-h-full max-w-full bg-amber-100 page-body"}>
+        <AboutContent />
+        <div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,21 +1,14 @@
-import Head from 'next/head'
 import HeaderNavBar from "@/components/HeaderNavBar";
 import {ContactContent} from "@/components/ContactContent";
 import styles from "@/styles/Home.module.css";
 
 export default function Contact() {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className={styles.main}>
-        <HeaderNavBar />
-        <div className={"h-fit w-full bg-amber-100 page-body"}>
-          <ContactContent />
-        </div>
+    <div className={styles.main}>
+      <HeaderNavBar />
+      <div className={"h-fit w-full bg-amber-100 page-body"}>
+        <ContactContent />
       </div>
-    </>
+    </div>
   )
 }
