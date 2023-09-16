97f77f7a547bbf9958f9044acf953bedcf825e41
HBU <henrik-gb@hotmail.com>
2023-09-16 12:34:13 +0200

    eslint formatting of HeaderNavBar.tsx

--- src/components/HeaderNavBar.tsx
+++ src/components/HeaderNavBar.tsx
@@ -3,47 +3,47 @@ import Link from "next/link";
 import {useState} from "react";
 
 export default function HeaderNavBar() {
-    const [isNavExpanded, setIsNavExpanded] = useState(false)
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
 
-    return (
-        <nav className="navigation">
-            <Link href="/" className="brand-name">
+  return (
+    <nav className="navigation">
+      <Link href="/" className="brand-name">
                 Kite Spots
-            </Link>
-            <button className="hamburger"
-                    onClick={() => {
-                        setIsNavExpanded(!isNavExpanded);
-                    }}>
-                {/* icon from heroicons.com */}
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="white"
-                >
-                    <path
-                        fillRule="evenodd"
-                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                        clipRule="evenodd"
-                    />
-                </svg>
-            </button>
-            <div
-                className={
-                 isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-                }>
-                <ul>
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/About">About</Link>
-                    </li>
-                    <li>
-                        <Link href="/Contact">Contact</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
+      </Link>
+      <button className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}>
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+        className={
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/About">About</Link>
+          </li>
+          <li>
+            <Link href="/Contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
