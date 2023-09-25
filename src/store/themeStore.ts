c67b8de9da5570d346c517b922e83b021bea1c45
HBU <henrik-gb@hotmail.com>
2023-09-25 09:00:11 +0200

    fix: Simplify logic in Zustand store. Set 'normal-mode' as default theme state, and remove complicated logic with localstorage. The previous solution was not stable, and the new solution is both more robust and simple.

--- src/store/themeStore.ts
+++ src/store/themeStore.ts
@@ -1,17 +1,8 @@
 // store.ts
 import { create } from 'zustand';
 
-
 // Define the possible theme values
 type Theme = 'normal-mode' | 'dark-mode';
-
-const getInitialTheme = (): Theme => {
-  if (typeof window !== 'undefined' && window.localStorage) {
-    const storedTheme = window.localStorage.getItem('theme');
-    return storedTheme === 'dark-mode' ? 'dark-mode' : 'normal-mode';
-  }
-  return 'normal-mode';
-};
 
 // Define the state's shape
 interface ThemeState {
@@ -20,12 +11,9 @@ interface ThemeState {
 }
 
 const useThemeStore = create<ThemeState>((set) => ({
-  theme: getInitialTheme(),
+  theme: 'normal-mode',
   toggleTheme: () => set((state) => {
     const newTheme: Theme = state.theme === 'normal-mode' ? 'dark-mode' : 'normal-mode';
-    if (typeof window !== 'undefined' && window.localStorage) {
-      window.localStorage.setItem('theme', newTheme);
-    }
     return { theme: newTheme };
   })
 }));
