92924f9b000ef11a7b38399f9dd4d1d06f645e23
HBU <henrik-gb@hotmail.com>
2023-10-31 21:15:16 +0100

    Fix: Remove unused imports

--- src/components/AboutPage/AboutContent.tsx
+++ src/components/AboutPage/AboutContent.tsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from "react";
 import useThemeStore from "@/store/themeStore";
 import { useTranslation } from "next-i18next";
-import axios from 'axios';
-import WindVsRain from "@/components/Charts/WindVsRain";
 
 export function AboutContent() {
   const { theme } = useThemeStore();
