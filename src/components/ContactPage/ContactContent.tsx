8638a6b378fb5e06406c6c18625b2fb411eadd75
HBU <henrik-gb@hotmail.com>
2023-11-07 22:08:24 +0100

    Feat: Add space between text paragrapgs in ContactContent.tsx

--- src/components/ContactPage/ContactContent.tsx
+++ src/components/ContactPage/ContactContent.tsx
@@ -10,7 +10,9 @@ export const ContactContent: React.FC = () => {
   return (
     <div className={`text-container ${theme}`}>
       <p className="font-bold text-2xl mb-4">{t('contactInfoHeader')}</p>
-      <Trans i18nKey="contactInfoMessagePart1" components={{ br: <br /> }}></Trans>
+      <div className="mb-4">
+        <Trans i18nKey="contactInfoMessagePart1" components={{ br: <br /> }}></Trans>
+      </div>
       <Trans i18nKey="contactInfoEmail" values={{ email }}>
         <a href={`mailto:${email}`} className={`font-bold text-link-color ${theme}`}></a>
       </Trans>
