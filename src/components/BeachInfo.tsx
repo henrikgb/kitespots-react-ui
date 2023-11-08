c604b0ed4ff756698b27ab1d20712583aefea372
HBU <henrik-gb@hotmail.com>
2023-11-08 12:10:56 +0100

    refacute: refacture index.tsx by moving parts of the code into a separate file

---
+++ src/components/BeachInfo.tsx
@@ -0,0 +1,59 @@
+import styleClasses from "@/pages/index.module.css";
+import {StarRating} from "@/util/StarRating";
+import React from "react";
+import useBeachDescriptionStore from "@/store/beachDescriptionStore";
+import {useTranslation} from "react-i18next";
+
+export const BeachInfo = () => {
+  const { t } = useTranslation();
+  const {
+    nameId,
+    beginnerScore,
+    freestyleScore,
+    waveScore} = useBeachDescriptionStore();
+  return (
+    <div className="p-5 flex flex-col gap-12 justify-center">
+      <p className={"font-bold text-3xl"}>{nameId ? (nameId?.charAt(0).toUpperCase() + nameId?.slice(1).toLowerCase()) : undefined}</p>
+      <div className={styleClasses.textList}>
+        <ul className="flex flex-col justify-center ">
+          <li>
+            <p>{t("sideOnshore")}</p>
+            <span className={styleClasses.square} style={{ backgroundColor: '#00bb00' }}></span>
+          </li>
+          <li>
+            <p>{t("onshore")}</p>
+            <span className={styleClasses.square} style={{ backgroundColor: '#008000' }}></span>
+          </li>
+          <li>
+            <p>{t("overLand")}</p>
+            <span className={styleClasses.square} style={{ backgroundColor: '#45a3ff' }}></span>
+          </li>
+          <li>
+            <p>{t("sideOffshore")}</p>
+            <span className={styleClasses.square} style={{ backgroundColor: '#ffa500' }}></span>
+          </li>
+          <li>
+            <p>{t("offshore")}</p>
+            <span className={styleClasses.square} style={{ backgroundColor: '#FD0100' }}></span>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li className="flex flex-row gap-2">
+            <p>{t("beginner")}:</p>
+            <StarRating score={beginnerScore ? beginnerScore : 0} />
+          </li>
+          <li className="flex flex-row gap-2">
+            <p>{t("freestyle")}:</p>
+            <StarRating score={freestyleScore ? freestyleScore : 0} />
+          </li>
+          <li className="flex flex-row gap-2">
+            <p>{t("wave")}:</p>
+            <StarRating score={waveScore ? waveScore : 0} />
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ import WindVsRain from "@/components/Charts/WindVsRain";
 import styleClasses from "@/pages/index.module.css";
 import WindDirection from "@/components/Charts/WindDirection";
 import useBeachDescriptionStore from "@/store/beachDescriptionStore";
-import {StarRating} from "@/util/StarRating";
 import {useMeteomaticsWeatherDataStore} from "@/store/meteomaticsWeatherDataStore";
+import {BeachInfo} from "@/components/BeachInfo";
 
 export default function Home() {
   const { setMeteomaticsData,
@@ -17,11 +17,7 @@ export default function Home() {
     windSpeed10ms,
     precipitation } = useMeteomaticsWeatherDataStore();
   const {
-    nameId,
     image,
-    beginnerScore,
-    freestyleScore,
-    waveScore,
     windDirectionDescriptions} = useBeachDescriptionStore();
 
   useEffect(() => {
@@ -44,49 +40,7 @@ export default function Home() {
         </div>
         <div className={`${styleClasses.descriptionContainer}`} >
           <Image src={image ? image : ""} alt="Beach Wind Directions" style={{ width: '54vh' }} />
-          <div className="p-5 flex flex-col gap-12 justify-center">
-            <p className={"font-bold text-3xl"}>{nameId ? (nameId?.charAt(0).toUpperCase() + nameId?.slice(1).toLowerCase()) : undefined}</p>
-            <div className={styleClasses.textList}>
-              <ul className="flex flex-col justify-center ">
-                <li>
-                  Side Onshore - Ideal
-                  <span className={styleClasses.square} style={{ backgroundColor: '#00bb00' }}></span>
-                </li>
-                <li>
-                  Onshore - Hard for beginners
-                  <span className={styleClasses.square} style={{ backgroundColor: '#008000' }}></span>
-                </li>
-                <li>
-                  Over land - Gusty
-                  <span className={styleClasses.square} style={{ backgroundColor: '#45a3ff' }}></span>
-                </li>
-                <li>
-                  Side Offshore - Dangerous
-                  <span className={styleClasses.square} style={{ backgroundColor: '#ffa500' }}></span>
-                </li>
-                <li>
-                  Offshore - Very Dangerous
-                  <span className={styleClasses.square} style={{ backgroundColor: '#FD0100' }}></span>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <ul>
-                <li className="flex flex-row gap-2">
-                  <p>Beginner:</p>
-                  <StarRating score={beginnerScore ? beginnerScore : 0} />
-                </li>
-                <li className="flex flex-row gap-2">
-                  <p>Freestyle:</p>
-                  <StarRating score={freestyleScore ? freestyleScore : 0} />
-                </li>
-                <li className="flex flex-row gap-2">
-                  <p>Wave:</p>
-                  <StarRating score={waveScore ? waveScore : 0} />
-                </li>
-              </ul>
-            </div>
-          </div>
+          <BeachInfo />
         </div>
       </div>
       <div  className={`${styleClasses.bottomCraphContainer}`}>
