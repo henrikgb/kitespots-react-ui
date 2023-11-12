06e8667c540400cd5425e2f382caa85ca2fc30a5
HBU <henrik-gb@hotmail.com>
2023-11-12 21:56:16 +0100

    Refactor text in PuffDataLoader.tsx

--- src/components/LoadingSpinners/PuffDataLoader.tsx
+++ src/components/LoadingSpinners/PuffDataLoader.tsx
@@ -10,7 +10,7 @@ export const PuffDataLoader = () => {
         aria-label="Loading Spinner"
         data-testid="loader"
       />
-      <p className={"font-bold text-sm"} style={{color: "cadetblue"}}>Data is loading</p>
+      <p className={"font-bold text-sm"} style={{color: "cadetblue"}}>Loading data</p>
     </div>
   )
 }
