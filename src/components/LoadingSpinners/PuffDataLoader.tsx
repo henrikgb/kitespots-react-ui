f0793eab459d0c0487815e3e645c736832197604
HBU <henrik-gb@hotmail.com>
2023-11-12 21:52:48 +0100

    Feat: Create reusable loader-spinner to visualise that data is loading

---
+++ src/components/LoadingSpinners/PuffDataLoader.tsx
@@ -0,0 +1,16 @@
+import PuffLoaderSpinner from "react-spinners/PuffLoader";
+import React from "react";
+
+export const PuffDataLoader = () => {
+  return(
+    <div className="h-full flex flex-col justify-center items-center">
+      <PuffLoaderSpinner
+        color={"cadetblue"}
+        size={100}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      <p className={"font-bold text-sm"} style={{color: "cadetblue"}}>Data is loading</p>
+    </div>
+  )
+}
