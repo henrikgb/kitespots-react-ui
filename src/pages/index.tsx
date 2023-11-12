5c4a36f715710bc1882fe6048189d1a7c63ab200
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2023-11-12 22:01:41 +0100

    Feat/add loading spinner (#35)
    - npm install --save react-spinners
    - create reusable loader-spinner to visualise that data is loading
    - add loading spinner to home page (index.tsx) to visualise when data is
    loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {KiteSpotsMap} from "@/components/MapPage/KiteSpotsMap";
 import WindVsRain from "@/components/Charts/WindVsRain";
@@ -7,8 +7,9 @@ import WindDirection from "@/components/Charts/WindDirection";
 import useBeachDescriptionStore from "@/store/beachDescriptionStore";
 import {useMeteomaticsWeatherDataStore} from "@/store/meteomaticsWeatherDataStore";
 import {BeachInfo} from "@/components/BeachInfo/BeachInfo";
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'; // Import this
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { GetStaticPropsContext } from 'next';
+import {PuffDataLoader} from "@/components/LoadingSpinners/PuffDataLoader";
 
 function Home() {
   const { setMeteomaticsData,
@@ -18,6 +19,7 @@ function Home() {
     windSpeed10ms,
     precipitation } = useMeteomaticsWeatherDataStore();
   const {windDirectionDescriptions} = useBeachDescriptionStore();
+  const [isloading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     axios.get('/api/meteomaticsData')
@@ -25,6 +27,7 @@ function Home() {
         const data = response.data.data;
         setMeteomaticsData(data);
         setSelectedLocation("SELE"); // Set default location to Selestranden
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching Meteomatics data:', error);
@@ -40,16 +43,26 @@ function Home() {
         <BeachInfo />
       </div>
       <div className={`${styleClasses.bottomCraphContainer}`}>
-        {(windGusts10ms && windSpeed10ms && precipitation) && (
-          <div className={`${styleClasses.windVsRainGraph as string}`}>
+        <div className={`${styleClasses.windVsRainGraph as string}`}>
+          {isloading && (
+            <div className="h-full flex flex-col justify-center items-center">
+              <PuffDataLoader />
+            </div>
+          )}
+          {(!isloading && windGusts10ms && windSpeed10ms && precipitation) && (
             <WindVsRain data={{ windGust: windGusts10ms[0].dates, windSpeed: windSpeed10ms[0].dates, precipitation: precipitation[0].dates }} />
-          </div>
-        )}
-        {windDirection10ms && windDirection10ms[0] && (
-          <div className={`${styleClasses.windDirectionGraph as string} p-5`}>
-            <WindDirection data={windDirection10ms[0].dates} windDirectionDescriptions={windDirectionDescriptions ? windDirectionDescriptions : []} />
-          </div>
-        )}
+          )}
+        </div>
+        <div className={`${styleClasses.windDirectionGraph as string} p-5`}>
+          {isloading && (
+            <div className="w-full h-full flex justify-center items-center">
+              <PuffDataLoader />
+            </div>
+          )}
+          {(!isloading && windDirection10ms && windDirection10ms[0]) && (
+            <WindDirection data={windDirection10ms[0].dates} windDirectionDescriptions={windDirectionDescriptions ? windDirectionDescriptions : []} />  
+          )}
+        </div>
       </div>
     </div>
   )
