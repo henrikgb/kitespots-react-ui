ccb6f8d2e8930a96ea98a2e4228da71410cd4005
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2023-12-10 23:04:51 +0100

    feat: display description text for wind direction in tooltip (#43)

--- src/assets/beachCoordinates.tsx
+++ src/assets/beachCoordinates.tsx
@@ -43,49 +43,49 @@ export const beachCoordinates: BeachCoordinateProp[] = [
         intervalStart: 0,
         intervalStop: 10,
         colorCode: "#45a3ff",
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 10,
         intervalStop: 60,
         colorCode: '#ffa500',
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 60,
         intervalStop: 150,
         colorCode: "#FD0100",
-        category: "very dangerous"
+        category: "offshore"
       },
       {
         intervalStart: 150,
         intervalStop: 190,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 190,
         intervalStop: 250,
         colorCode: '#45a3ff',
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 250,
         intervalStop: 265,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 265,
         intervalStop: 305,
         colorCode: '#008000',
-        category: "hard for beginners"
+        category: "onshore"
       },
       {
         intervalStart: 305,
         intervalStop: 360,
         colorCode: '#45a3ff',
-        category: "gusty"
+        category: "overLand"
       }
     ]
   },
@@ -103,55 +103,55 @@ export const beachCoordinates: BeachCoordinateProp[] = [
         intervalStart: 0,
         intervalStop: 10,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 10,
         intervalStop: 140,
         colorCode: '#FD0100',
-        category: "very dangerous"
+        category: "offshore"
       },
       {
         intervalStart: 140,
         intervalStop: 165,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 165,
         intervalStop: 225,
         colorCode: "#45a3ff",
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 225,
         intervalStop: 235,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 235,
         intervalStop: 275,
         colorCode: '#008000',
-        category: "hard for beginners"
+        category: "onshore"
       },
       {
         intervalStart: 275,
         intervalStop: 315,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 315,
         intervalStop: 345,
         colorCode: '#45a3ff',
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 345,
         intervalStop: 360,
         colorCode: '#ffa500',
-        category: "dangerous"
+        category: "sideOffshore"
       }
     ]
   },
@@ -169,55 +169,55 @@ export const beachCoordinates: BeachCoordinateProp[] = [
         intervalStart: 0,
         intervalStop: 10,
         colorCode: "#45a3ff",
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 10,
         intervalStop: 30,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 30,
         intervalStop: 160,
         colorCode: '#FD0100',
-        category: "very dangerous"
+        category: "offshore"
       },
       {
         intervalStart: 160,
         intervalStop: 190,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 190,
         intervalStop: 210,
         colorCode: "#45a3ff",
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 210,
         intervalStop: 270,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 270,
         intervalStop: 310,
         colorCode: '#008000',
-        category: "hard for beginners"
+        category: "onshore"
       },
       {
         intervalStart: 310,
         intervalStop: 320,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 320,
         intervalStop: 360,
         colorCode: '#45a3ff',
-        category: "gusty"
+        category: "overLand"
       }
     ]
   },
@@ -235,55 +235,55 @@ export const beachCoordinates: BeachCoordinateProp[] = [
         intervalStart: 0,
         intervalStop: 20,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 20,
         intervalStop: 150,
         colorCode: '#FD0100',
-        category: "very dangerous"
+        category: "offshore"
       },
       {
         intervalStart: 150,
         intervalStop: 170,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 170,
         intervalStop: 210,
         colorCode: "#45a3ff",
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 210,
         intervalStop: 240,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 240,
         intervalStop: 280,
         colorCode: '#008000',
-        category: "hard for beginners"
+        category: "onshore"
       },
       {
         intervalStart: 280,
         intervalStop: 330,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 330,
         intervalStop: 350,
         colorCode: '#45a3ff',
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 350,
         intervalStop: 360,
         colorCode: '#ffa500',
-        category: "dangerous"
+        category: "sideOffshore"
       }
     ]
   },
@@ -301,55 +301,55 @@ export const beachCoordinates: BeachCoordinateProp[] = [
         intervalStart: 0,
         intervalStop: 10,
         colorCode: "#45a3ff",
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 10,
         intervalStop: 20,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 20,
         intervalStop: 160,
         colorCode: '#FD0100',
-        category: "very dangerous"
+        category: "offshore"
       },
       {
         intervalStart: 160,
         intervalStop: 175,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 175,
         intervalStop: 220,
         colorCode: "#45a3ff",
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 220,
         intervalStop: 255,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 255,
         intervalStop: 295,
         colorCode: '#008000',
-        category: "hard for beginners"
+        category: "onshore"
       },
       {
         intervalStart: 295,
         intervalStop: 340,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 340,
         intervalStop: 360,
         colorCode: '#45a3ff',
-        category: "gusty"
+        category: "overLand"
       }
     ]
   },
@@ -367,55 +367,55 @@ export const beachCoordinates: BeachCoordinateProp[] = [
         intervalStart: 0,
         intervalStop: 135,
         colorCode: '#FD0100',
-        category: "very dangerous"
+        category: "offshore"
       },
       {
         intervalStart: 135,
         intervalStop: 160,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 160,
         intervalStop: 180,
         colorCode: "#45a3ff",
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 180,
         intervalStop: 235,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 235,
         intervalStop: 275,
         colorCode: '#008000',
-        category: "hard for beginners"
+        category: "onshore"
       },
       {
         intervalStart: 275,
         intervalStop: 315,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 315,
         intervalStop: 345,
         colorCode: '#45a3ff',
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 345,
         intervalStop: 355,
         colorCode: '#ffa500',
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 355,
         intervalStop: 360,
         colorCode: '#FD0100',
-        category: "very dangerous"
+        category: "offshore"
       }
     ]
   },
@@ -433,55 +433,55 @@ export const beachCoordinates: BeachCoordinateProp[] = [
         intervalStart: 0,
         intervalStop: 5,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 5,
         intervalStop: 160,
         colorCode: '#FD0100',
-        category: "very dangerous"
+        category: "offshore"
       },
       {
         intervalStart: 160,
         intervalStop: 170,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 170,
         intervalStop: 185,
         colorCode: "#45a3ff",
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 185,
         intervalStop: 245,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 245,
         intervalStop: 285,
         colorCode: '#008000',
-        category: "hard for beginners"
+        category: "onshore"
       },
       {
         intervalStart: 285,
         intervalStop: 330,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 330,
         intervalStop: 355,
         colorCode: '#45a3ff',
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 355,
         intervalStop: 360,
         colorCode: '#ffa500',
-        category: "dangerous"
+        category: "sideOffshore"
       }
     ]
   },
@@ -499,55 +499,55 @@ export const beachCoordinates: BeachCoordinateProp[] = [
         intervalStart: 0,
         intervalStop: 30,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 30,
         intervalStop: 155,
         colorCode: '#FD0100',
-        category: "very dangerous"
+        category: "offshore"
       },
       {
         intervalStart: 155,
         intervalStop: 180,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 180,
         intervalStop: 200,
         colorCode: "#45a3ff",
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 200,
         intervalStop: 270,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 270,
         intervalStop: 310,
         colorCode: '#008000',
-        category: "hard for beginners"
+        category: "onshore"
       },
       {
         intervalStart: 310,
         intervalStop: 330,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 330,
         intervalStop: 352,
         colorCode: '#45a3ff',
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 352,
         intervalStop: 360,
         colorCode: '#ffa500',
-        category: "dangerous"
+        category: "sideOffshore"
       }
     ]
   },
@@ -565,55 +565,55 @@ export const beachCoordinates: BeachCoordinateProp[] = [
         intervalStart: 0,
         intervalStop: 95,
         colorCode: '#FD0100',
-        category: "very dangerous"
+        category: "offshore"
       },
       {
         intervalStart: 95,
         intervalStop: 105,
         colorCode: "#ffa500",
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 105,
         intervalStop: 150,
         colorCode: "#45a3ff",
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 150,
         intervalStop: 175,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 175,
         intervalStop: 215,
         colorCode: '#008000',
-        category: "hard for beginners"
+        category: "onshore"
       },
       {
         intervalStart: 215,
         intervalStop: 260,
         colorCode: '#00bb00',
-        category: "ideal"
+        category: "sideOnshore"
       },
       {
         intervalStart: 260,
         intervalStop: 285,
         colorCode: '#45a3ff',
-        category: "gusty"
+        category: "overLand"
       },
       {
         intervalStart: 285,
         intervalStop: 315,
         colorCode: '#ffa500',
-        category: "dangerous"
+        category: "sideOffshore"
       },
       {
         intervalStart: 315,
         intervalStop: 360,
         colorCode: '#FD0100',
-        category: "very dangerous"
+        category: "offshore"
       }
     ]
   }
