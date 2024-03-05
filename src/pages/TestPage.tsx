42730c675dc0d2adcf1e0c98637e72338f2ee767
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-03-05 20:41:20 +0100

    feat: Add buttons to switch landing page content view

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -5,20 +5,26 @@ import Image from "next/image";
 import sunsetImage from "@/assets/images/beachSunset.jpg";
 import {Trans, useTranslation} from "next-i18next";
 import PageWrapper from "@/components/PageWrapper";
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import usei18LanguageStore from "@/store/i18languageStore";
 import {KiteSpotsMap} from "@/components/map/KiteSpotsMap";
 
 export default function TestPage() {
-  const email = "henrik.busengdal@kitespots.no";
   const { t, i18n } = useTranslation();
   const {activeLanguage} = usei18LanguageStore();
+  const [activeLandingPageView, setActiveLandingPageView] = useState<"info" | "weather">("info");
 
   useEffect(() => {
     i18n.changeLanguage(activeLanguage);
   }, [i18n, activeLanguage]);
 
- 
+  const activateInfoPage = () => {
+    setActiveLandingPageView("info");
+  }
+  const activateWeatherPage = () => {
+    setActiveLandingPageView("weather");
+  }
+
   return(
     <PageWrapper>
       <Card className="mt-6 w-full bg-webPageContainerBody">
@@ -32,17 +38,21 @@ export default function TestPage() {
         <CardBody className="flex justify-start">
           <div className="h-full">
             <div className="flex gap-4">
-              <IconButton>
-                <i className="fas fa-heart" />
+              <IconButton size="lg" color={activeLandingPageView === "info" ? "blue-gray" : "white"} onClick={activateInfoPage}>
+                <div className="flex w-full flex-col justify-center items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                    <path fillRule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-7-4a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM9 9a.75.75 0 0 0 0 1.5h.253a.25.25 0 0 1 .244.304l-.459 2.066A1.75 1.75 0 0 0 10.747 15H11a.75.75 0 0 0 0-1.5h-.253a.25.25 0 0 1-.244-.304l.459-2.066A1.75 1.75 0 0 0 9.253 9H9Z" clipRule="evenodd" />
+                  </svg>
+                  <p style={{fontSize: 9}}>Info</p>
+                </div>
               </IconButton>
-              <IconButton variant="gradient">
-                <i className="fas fa-heart" />
-              </IconButton>
-              <IconButton variant="outlined">
-                <i className="fas fa-heart" />
-              </IconButton>
-              <IconButton variant="text">
-                <i className="fas fa-heart" />
+              <IconButton size="lg" color={activeLandingPageView === "weather" ? "blue-gray" : "white"} onClick={activateWeatherPage}>
+                <div className="flex w-full flex-col justify-center items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                    <path d="M15.5 2A1.5 1.5 0 0 0 14 3.5v13a1.5 1.5 0 0 0 1.5 1.5h1a1.5 1.5 0 0 0 1.5-1.5v-13A1.5 1.5 0 0 0 16.5 2h-1ZM9.5 6A1.5 1.5 0 0 0 8 7.5v9A1.5 1.5 0 0 0 9.5 18h1a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 10.5 6h-1ZM3.5 10A1.5 1.5 0 0 0 2 11.5v5A1.5 1.5 0 0 0 3.5 18h1A1.5 1.5 0 0 0 6 16.5v-5A1.5 1.5 0 0 0 4.5 10h-1Z" />
+                  </svg>
+                  <p style={{fontSize: 9}}>Weather</p>
+                </div>
               </IconButton>
             </div>
           </div>
