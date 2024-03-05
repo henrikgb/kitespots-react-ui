a365709ee54ac8c20a0fd2c22f1f2cccc1babef2
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-03-05 23:49:56 +0100

    feat: update navigation bar styling

--- src/components/headerNavigationBar/HeaderNavBar.tsx
+++ src/components/headerNavigationBar/HeaderNavBar.tsx
@@ -24,48 +24,52 @@ export default function HeaderNavBar() {
   }
 
   return (
-    <nav className="navigation">
-      <div className="flex mx-4">
-        <Image src={KitespotsLogoSvg || ""} alt="Beach Wind Directions" style={{height: 50, width: 50}}/>
+    <div className="navigation">
+      <div className="w-full max-w-[1280px]">
+        <nav className="navContainer">
+          <div className="flex mx-4">
+            <Image src={KitespotsLogoSvg || ""} alt="Beach Wind Directions" style={{height: 50, width: 50}}/>
+          </div>
+          <Link href="/" className="brand-name">Kite Spots</Link>
+          <button className="hamburger"
+            onClick={() => {
+              setIsNavExpanded(!isNavExpanded);
+            }}>
+            {/* icon from heroicons.com */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="white"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+          <div
+            className={
+              isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+            }>
+            <ul>
+              <li>
+                <Link onClick={closeNav} href="/">{t("home")}</Link>
+              </li>
+              <li>
+                <Link onClick={closeNav} href="/aboutPage/AboutContent">{t("about")}</Link>
+              </li>
+              <li>
+                <Link onClick={closeNav} href="/contactPage/ContactContent">{t("contact")}</Link>
+              </li>
+              <li>
+                <Link onClick={closeNav} href="/settingsPage/SettingsContent">{t("settings")}</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
-      <Link href="/" className="brand-name">Kite Spots</Link>
-      <button className="hamburger"
-        onClick={() => {
-          setIsNavExpanded(!isNavExpanded);
-        }}>
-        {/* icon from heroicons.com */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="white"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
-      <div
-        className={
-          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-        }>
-        <ul>
-          <li>
-            <Link onClick={closeNav} href="/">{t("home")}</Link>
-          </li>
-          <li>
-            <Link onClick={closeNav} href="/aboutPage/AboutContent">{t("about")}</Link>
-          </li>
-          <li>
-            <Link onClick={closeNav} href="/contactPage/ContactContent">{t("contact")}</Link>
-          </li>
-          <li>
-            <Link onClick={closeNav} href="/settingsPage/SettingsContent">{t("settings")}</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    </div>
   );
 }
