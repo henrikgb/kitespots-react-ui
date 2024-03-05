0d995e3055027b8e3bf85a5bff3dc175115ca139
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-03-05 23:21:56 +0100

    feat: update BeachInfo.tsx

--- src/components/beachInfo/BeachInfo.tsx
+++ src/components/beachInfo/BeachInfo.tsx
@@ -21,52 +21,47 @@ export const BeachInfo = () => {
   }, [i18n, activeLanguage]);
 
   return (
-    <div className="flex flex-col gap-4 p-5 w-full">
-      <div className="flex justify-center w-full">
-        <p className={"flex justify-center font-bold text-3xl w-fit"} style={{borderBottom: "solid black"}}>{nameId ? (nameId?.charAt(0).toUpperCase() + nameId?.slice(1).toLowerCase()) : undefined}</p>
-      </div>
-      <div className="flex justify-center">
+    <div className="flex flex-row gap-4 w-full h-full justify-center items-center flex-wrap">
+      <div className="flex justify-center items-center">
         <ul>
-          <li className="flex flex-row gap-2">
-            <p>{t("beginner")}:</p>
+          <li className="flex flex-row items-center gap-2">
+            <p style={{fontSize: 12, marginBottom: 0}}>{t("beginner")}:</p>
             <StarRating score={beginnerScore ? beginnerScore : 0} />
           </li>
-          <li className="flex flex-row gap-2">
-            <p>{t("freestyle")}:</p>
+          <li className="flex flex-row items-center gap-2">
+            <p style={{fontSize: 12, marginBottom: 0}}>{t("freestyle")}:</p>
             <StarRating score={freestyleScore ? freestyleScore : 0} />
           </li>
-          <li className="flex flex-row gap-2">
-            <p>{t("wave")}:</p>
+          <li className="flex flex-row items-center gap-2">
+            <p style={{fontSize: 12, marginBottom: 0}}>{t("wave")}:</p>
             <StarRating score={waveScore ? waveScore : 0} />
           </li>
         </ul>
       </div>
-      <div className={styleClasses.beachInfoContainer}>
-        <Image className="flex items-start" src={image ? image : ""} alt="Beach Wind Directions" style={{ width: '34vh' }} />
-        <div className={styleClasses.textList}>
-          <ul className="flex flex-col justify-center ml-4">
-            <li>
-              <p>{t("sideOnshore")}</p>
-              <span className={styleClasses.square} style={{ backgroundColor: '#00bb00' }}></span>
-            </li>
-            <li>
-              <p>{t("onshore")}</p>
-              <span className={styleClasses.square} style={{ backgroundColor: '#008000' }}></span>
-            </li>
-            <li>
-              <p>{t("overLand")}</p>
-              <span className={styleClasses.square} style={{ backgroundColor: '#45a3ff' }}></span>
-            </li>
-            <li>
-              <p>{t("sideOffshore")}</p>
-              <span className={styleClasses.square} style={{ backgroundColor: '#ffa500' }}></span>
-            </li>
-            <li>
-              <p>{t("offshore")}</p>
-              <span className={styleClasses.square} style={{ backgroundColor: '#FD0100' }}></span>
-            </li>
-          </ul>
-        </div>
+      <Image className="flex items-start" src={image ? image : ""} alt="Beach Wind Directions" style={{ width: '34vh' }} />
+      <div className={styleClasses.textList}>
+        <ul className="flex flex-col justify-center ml-4">
+          <li>
+            <p style={{fontSize: 12}}>{t("sideOnshore")}</p>
+            <span className={styleClasses.square} style={{ backgroundColor: '#00bb00', blockSize: 12 }}></span>
+          </li>
+          <li>
+            <p style={{fontSize: 12}}>{t("onshore")}</p>
+            <span className={styleClasses.square} style={{ backgroundColor: '#008000', blockSize: 12  }}></span>
+          </li>
+          <li>
+            <p style={{fontSize: 12}}>{t("overLand")}</p>
+            <span className={styleClasses.square} style={{ backgroundColor: '#45a3ff', blockSize: 12  }}></span>
+          </li>
+          <li>
+            <p style={{fontSize: 12}}>{t("sideOffshore")}</p>
+            <span className={styleClasses.square} style={{ backgroundColor: '#ffa500', blockSize: 12  }}></span>
+          </li>
+          <li>
+            <p style={{fontSize: 12}}>{t("offshore")}</p>
+            <span className={styleClasses.square} style={{ backgroundColor: '#FD0100', blockSize: 12  }}></span>
+          </li>
+        </ul>
       </div>
     </div>
   )
