fae86a66d1ba477dc145ec6ea968190e0b4383f8
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-05-31 23:28:38 +0200

    Update defaultLocale in next-18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    defaultLocale: 'nb',
+    defaultLocale: 'en',
     locales: ['en', 'nb'],
   },
   localePath: path.resolve('./public/locales'),
