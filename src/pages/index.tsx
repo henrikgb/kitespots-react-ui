abc0d6d1ae572c3f5ba02ceee456489145707e59
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-06-01 00:37:42 +0200

    refactor index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,15 +23,15 @@ function Home() {
   const {nameId, windDirectionDescriptions} = useBeachDescriptionStore();
   const [activeLandingPageView, setActiveLandingPageView] = useState<"info" | "weather">("info");
   const { t} = useTranslation();
-
+  
   useMeteomaticsWeatherData();
 
   useActiveLanguage();
 
-  const activateInfoPage = () => {
+  const setInfoPageToActive = () => {
     setActiveLandingPageView("info");
   }
-  const activateWeatherPage = () => {
+  const setWeatherPageToActivate = () => {
     setActiveLandingPageView("weather");
   }
 
@@ -47,7 +47,7 @@ function Home() {
         </CardHeader>
         <CardBody className="flex flex-col gap-4 justify-start">
           <div className="flex gap-4">
-            <IconButton size="lg" color={activeLandingPageView === "info" ? "blue-gray" : "white"} onClick={activateInfoPage}>
+            <IconButton size="lg" color={activeLandingPageView === "info" ? "blue-gray" : "white"} onClick={setInfoPageToActive}>
               <div className="flex w-full flex-col justify-center items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                   <path fillRule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-7-4a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM9 9a.75.75 0 0 0 0 1.5h.253a.25.25 0 0 1 .244.304l-.459 2.066A1.75 1.75 0 0 0 10.747 15H11a.75.75 0 0 0 0-1.5h-.253a.25.25 0 0 1-.244-.304l.459-2.066A1.75 1.75 0 0 0 9.253 9H9Z" clipRule="evenodd" />
@@ -55,7 +55,7 @@ function Home() {
                 <p style={{fontSize: 9}}>{t("info")}</p>
               </div>
             </IconButton>
-            <IconButton size="lg" color={activeLandingPageView === "weather" ? "blue-gray" : "white"} onClick={activateWeatherPage}>
+            <IconButton size="lg" color={activeLandingPageView === "weather" ? "blue-gray" : "white"} onClick={setWeatherPageToActivate}>
               <div className="flex w-full flex-col justify-center items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                   <path d="M15.5 2A1.5 1.5 0 0 0 14 3.5v13a1.5 1.5 0 0 0 1.5 1.5h1a1.5 1.5 0 0 0 1.5-1.5v-13A1.5 1.5 0 0 0 16.5 2h-1ZM9.5 6A1.5 1.5 0 0 0 8 7.5v9A1.5 1.5 0 0 0 9.5 18h1a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 10.5 6h-1ZM3.5 10A1.5 1.5 0 0 0 2 11.5v5A1.5 1.5 0 0 0 3.5 18h1A1.5 1.5 0 0 0 6 16.5v-5A1.5 1.5 0 0 0 4.5 10h-1Z" />
@@ -65,11 +65,12 @@ function Home() {
             </IconButton>
             <p className={"flex justify-center font-bold text-3xl w-fit"} style={{borderBottom: "solid black"}}>{nameId ? (nameId?.charAt(0).toUpperCase() + nameId?.slice(1).toLowerCase()) : undefined}</p>
           </div>
-          {(activeLandingPageView === "info") ? (
+          {(activeLandingPageView === "info") && (
             <div>
               <BeachInfo />
             </div>
-          ): (
+          )}
+          {(activeLandingPageView === "weather") && (
             <div className="flex flex-col gap-4">
               {isMeteomaticsDataLoading && (
                 <PuffDataLoader />
