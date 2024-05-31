e7f848b3403769d93dcfc4d4f171485508716d52
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-06-01 00:29:25 +0200

    fix: remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
-import React, {useEffect, useState} from "react";
-import axios from "axios";
+import React, {useState} from "react";
 import {KiteSpotsMap} from "@/components/map/KiteSpotsMap";
 import WindVsRain from "@/components/charts/WindVsRain";
 import WindDirection from "@/components/charts/WindDirection";
