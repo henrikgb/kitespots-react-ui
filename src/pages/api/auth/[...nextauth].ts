a84614cbea10f46f5114b1b46fe1cc630e7fd598
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-10-19 02:19:41 +0200

    Updating environment variables without the prefix NEXT_PUBLIC_

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,9 +4,9 @@ import AzureADProvider from "next-auth/providers/azure-ad"
 export const authOptions = {
   providers: [
     AzureADProvider({
-      clientId: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID as string,
-      clientSecret: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_SECRET as string,
-      tenantId: process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID as string,
+      clientId: process.env.AZURE_AD_CLIENT_ID as string,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET as string,
+      tenantId: process.env.AZURE_AD_TENANT_ID as string,
       name: "Kitespots Login",
     }),
   ],
